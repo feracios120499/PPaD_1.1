ff083ed5b56a8ac4ae28ff7046da8a846f66b3aa
SolMax Feracios <holyvood12@mail.ru>
2019-09-04 22:22:19 +0300

    add cesar and slogan chiper

---
+++ CesarChiper/CesarChiper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarChiperLibrary
+{
+    public class CesarChiper
+    {
+        //символы русской азбуки
+        const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        public CesarChiper(int Key)
+        {
+            this.Key = Key;
+        }
+        public int Key { get; set; }
+        private string CodeEncode(string text, int key)
+        {
+            //добавляем в алфавит маленькие буквы
+            var fullAlfabet = alfabet + alfabet.ToLower();
+            var letterQty = fullAlfabet.Length;
+            var retVal = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                var index = fullAlfabet.IndexOf(c);
+                if (index < 0)
+                {
+                    //если символ не найден, то добавляем его в неизменном виде
+                    retVal += c.ToString();
+                }
+                else
+                {
+                    var codeIndex = (letterQty + index + key) % letterQty;
+                    retVal += fullAlfabet[codeIndex];
+                }
+            }
+
+            return retVal;
+        }
+
+        //шифрование текста
+        public string Encrypt(string plainMessage)
+            => CodeEncode(plainMessage, Key);
+
+        //дешифрование текста
+        public string Decrypt(string encryptedMessage)
+            => CodeEncode(encryptedMessage, -Key);
+    }
+}
---
+++ SloganChiperLibrary/SloganChiper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloganChiperLibrary
+{
+    public class SloganCipher
+    {
+        const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        string table;
+
+        public SloganCipher(string slogan)
+        {
+            this.Slogan = slogan ?? throw new ArgumentNullException("slogan");
+            CreateTable();
+        }
+
+        public string Slogan
+        {
+            get;
+            private set;
+        }
+
+        public string Encrypt(string plainMessage)
+        {
+            if (string.IsNullOrEmpty(plainMessage)) return string.Empty;
+
+            string resultStr = string.Empty;
+            var fullAlfabet = alfabet + alfabet.ToLower();
+            foreach (var letter in plainMessage)
+            {
+                var index = fullAlfabet.IndexOf(letter);
+                if (index < 0)
+                {
+                    //если символ не найден, то добавляем его в неизменном виде
+                    resultStr += letter.ToString();
+                }
+                else
+                {
+                    resultStr += table[index];
+                }
+
+            }
+            return resultStr;
+        }
+
+        public string Decrypt(string encryptedMessage)
+        {
+            if (string.IsNullOrEmpty(encryptedMessage)) return string.Empty;
+
+            string resultStr = string.Empty;
+            var fullAlfabet = alfabet + alfabet.ToLower();
+            foreach (var letter in encryptedMessage)
+            {
+                var index = table.IndexOf(letter);
+                if (index < 0)
+                {
+                    //если символ не найден, то добавляем его в неизменном виде
+                    resultStr += letter.ToString();
+                }
+                else
+                {
+                    resultStr += fullAlfabet[index];
+                }
+
+            }
+            return resultStr;
+        }
+
+        private void CreateTable()
+        {
+            table = String.Concat(Slogan.ToUpper().Where(IsRussianLetter).Distinct().Concat(alfabet.Except(Slogan)));
+            table += table.ToLower();
+        }
+        private bool IsRussianLetter(char character)
+        {
+            return char.IsLetter(character) && character >= 'A' && character <= 'я';
+        }
+    }
+}
